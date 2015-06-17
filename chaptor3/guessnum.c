58066ce866271d55314cb51ca98e60d8d958a670
sunhao <sunhao_1988@msn.cn>
2015-06-17 16:32:04 -0700

    fix bug for guessnum

--- chaptor3/guessnum.c
+++ chaptor3/guessnum.c
@@ -9,7 +9,7 @@ void print_result();
 
 int main() {
 	guess_times = 0;
-	max_num = 10;
+	max_num = 100;
 	srand(time(NULL));
 	int num = rand() % max_num+1;	 
 	int user_guess;
@@ -19,24 +19,17 @@ int main() {
 			printf("print a number between 0 - %u:", max_num);
 			first_guess = 0;
 		} else {
-			printf("\n");
-			if(user_guess == num) {
-				printf("gueess");
-				print_result();
-			} else {
-				printf("your guess is %s than the number, again:", user_guess < num ? "smaller":"bigger"); 
-			}
+			printf("your guess is %s than the number, again:", user_guess < num ? "smaller":"bigger"); 
 		}
 		scanf("%u", &user_guess);
 		guess_times++;
 	}
 	while(user_guess != num);
+	print_result();
 }
 
 void print_result() {
-	printf("asdf%i\n", max_num);
 	int expect_times = ceil(log(max_num) / log(2));
-	printf("expect_times:%u, %u", expect_times, guess_times);	
 	if(guess_times > expect_times) {
 		printf("your guess times is %u, you are a pig.\n", guess_times);	} else {
 		printf("your guess times is %u, you are a genius.\n", guess_times); 
